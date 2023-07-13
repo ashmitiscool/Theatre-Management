0694ab7648c1730e225a5c099a139c5d94a51b56
Ashmit Verma <ashmitv444@gmail.com>
2023-07-13 22:21:40 +0530

    in rcmmdedMovieCode.py Made it show all movies when searched and if some banners empty, displays at random

--- rcmmdedMovieCode.py
+++ rcmmdedMovieCode.py
@@ -5,6 +5,7 @@ from PyQt5.QtCore import QStringListModel
 from PyQt5.QtWidgets import QCompleter
 from PyQt5.QtGui import QPixmap
 import PyQt5
+import random
 import sys
 
 class Ui_recommended(QWidget):
@@ -17,8 +18,24 @@ class Ui_recommended(QWidget):
         # Defining Widgets
         self.movieSearchBox = self.findChild(QLineEdit, 'movieSearchBox')
         self.searchButton = self.findChild(QPushButton, 'searchButton')
+
+        # Movie Labels
         self.movieLabel1 = self.findChild(QLabel, 'movieLabel1')
+        self.movieLabel2 = self.findChild(QLabel, 'movieLabel2')
+        self.movieLabel3 = self.findChild(QLabel, 'movieLabel3')
+        self.movieLabel4 = self.findChild(QLabel, 'movieLabel4')
+        self.movieLabel5 = self.findChild(QLabel, 'movieLabel5')
+        self.movieLabel6 = self.findChild(QLabel, 'movieLabel6')
+
+        # Movie Names
         self.movieName1 = self.findChild(QLabel, 'movieName1')
+        self.movieName2 = self.findChild(QLabel, 'movieName2')
+        self.movieName3 = self.findChild(QLabel, 'movieName3')
+        self.movieName4 = self.findChild(QLabel, 'movieName4')
+        self.movieName5 = self.findChild(QLabel, 'movieName5')
+        self.movieName6 = self.findChild(QLabel, 'movieName6')
+
+        self.backButton = self.findChild(QPushButton, 'backButton')
         #Adds search icon to button(If error, delete the two lines below this comment)
         self.searchButton.setIcon(QtGui.QIcon('Pictures\\srch.png'))
         self.searchButton.setIconSize(QtCore.QSize(16,16))
@@ -27,31 +44,69 @@ class Ui_recommended(QWidget):
 
         # Connecting buttons and stuff
         self.searchButton.clicked.connect(lambda:self.displayMovies())
+        self.backButton.clicked.connect(lambda:self.openHomePageWindow())
 
         # Create a QStringListModel and a QCompleter
         self.model = QStringListModel()
-        # List of movies here (Should be extracted from SQL)
+        # List of movie names here (Should be extracted from SQL)
         self.movieList = ['Avatar', 'Titanic', 'The Godfather', 'The Dark Knight', 'Inception']
         self.model.setStringList(self.movieList)
 
+        # Completer stuff
         self.completer = QCompleter()
         self.completer.setModel(self.model)
         self.completer.setCaseSensitivity(PyQt5.QtCore.Qt.CaseInsensitive)
         self.completer.setFilterMode(PyQt5.QtCore.Qt.MatchContains)  # Set filter mode to match substrings
 
-
         # Set the model and the completer for the movieSearchBox
         self.movieSearchBox.setCompleter(self.completer)
+        # List of movies banners here (Should be extracted from SQL)
+        self.imageList = {'Avatar':"C:\\Users\\ashmi\\Downloads\\projectImages\\lambos2.jpg",
+                          'Titanic':'C:\\Users\\ashmi\\Downloads\\projectImages\\lambos2.jpg',
+                          'The Godfather':'C:\\Users\\ashmi\\Downloads\\projectImages\\lambos2.jpg',
+                          'The Dark Knight':'C:\\Users\\ashmi\\Downloads\\projectImages\\lambos2.jpg',
+                          'Inception':'C:\\Users\\ashmi\\Downloads\\projectImages\\lambos2.jpg'}
 
-        self.imageList = {'Avatar':"C:\\Users\\ashmi\\Downloads\\projectImages\\lambos2.jpg"}
-
+    # This is the point which marks the place to undo code XD
     def displayMovies(self):
         # Extracting movie from the LineEdit
-        mov = self.movieSearchBox.text()
-        if mov in self.movieList:
-            pixmap = QPixmap(self.imageList[mov])
-            self.movieLabel1.setPixmap(pixmap)
-            self.movieName1.setText(mov)
+        search_text = self.movieSearchBox.text()
+        matching_movies = [movie for movie in self.movieList if search_text.upper() in movie.upper()]
+
+        for i in range(1, 7):  # Assuming you have 6 labels and buttons
+            if i <= len(matching_movies):
+                try:
+                    pixmap = QPixmap(self.imageList[matching_movies[i-1]])
+                    label = getattr(self, f"movieLabel{i}")
+                    name = getattr(self, f"movieName{i}")
+                    label.setPixmap(pixmap)
+                    name.setText(matching_movies[i-1])
+                except IndexError:
+                    # Handle the case where there are less than 6 matching movies
+                    print('Error1')
+                    break
+            else:
+                try:
+                    # Display random movies if no matching movies found
+                    random_movie = random.choice(self.movieList)
+                    print(random_movie)
+                    # Error in the following line
+                    pixmap = QPixmap(self.imageList[random_movie])
+
+                    label = getattr(self, f"movieLabel{i}")
+                    name = getattr(self, f"movieName{i}")
+                    print(label,name)
+                    label.setPixmap(pixmap)
+                    name.setText(random_movie)
+                except:
+                    print('Error2')
+
+
+    def openHomePageWindow(self):
+        from menuCode import Ui_Menu
+        self.close()
+        self.menu_window = Ui_Menu()
+        self.menu_window.show()
 
 
 
