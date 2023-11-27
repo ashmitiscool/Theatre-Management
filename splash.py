06f3ba33db640996e73851529cea123557948907
Shiv <117908887+LightnX222@users.noreply.github.com>
2023-11-27 22:57:03 +0530

    Splash Screen(Plz read desc)
    Start from splash.py, not startingprog.py

---
+++ splash.py
@@ -0,0 +1,42 @@
+import sys
+from PyQt5.QtWidgets import QDialog, QApplication, QSplashScreen
+from PyQt5.uic import loadUi
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QGridLayout, QWidget, QDesktopWidget
+import time
+
+class SplashScreen(QSplashScreen):
+    def __init__(self):
+        super(QSplashScreen,self).__init__()
+        loadUi('splash.ui',self)
+        qtRectangle = self.frameGeometry()
+        centerPoint = QDesktopWidget().availableGeometry().center()
+        qtRectangle.moveCenter(centerPoint)
+        self.move(qtRectangle.topLeft())   
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        
+        
+    def forward(self):
+        for i in range(0,100):
+                time.sleep(0.1)
+                self.progressBar.setValue(i)
+                
+class Ui_starting():
+    def __init__(self):
+        super(Ui_starting, self).__init__()
+
+        from homepageCode import Ui_HomePage
+
+        self.menu_window = Ui_HomePage()
+        self.menu_window.show()
+               
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+     
+    splash = SplashScreen()
+    splash.show()
+    splash.forward()
+    UIWindow = Ui_starting()
+    splash.hide()
+    splash.finish(self,UIWindow)
+    app.exec_()
